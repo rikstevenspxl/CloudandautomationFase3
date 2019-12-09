66b0646947c45cd4ae62d47a1654ccfc3cb486e2
rikstevenspxl <rik.stevens@hotmail.com>
2019-12-09 09:18:43 +0100

    Update
    rechttrekking

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,7 @@
-resource "aws_vpc" "Default-VPC" {
+resource "aws_vpc" "Fase3vpc" {
     cidr_block           = "172.31.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    instance_tenancy     = "default"
 
     tags= {
-        Name = "default"
+        Name = "Fase3vpc"
     }
 }
