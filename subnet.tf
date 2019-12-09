ce9d0f6686322ec35bd77cb63613733156033089
rikstevenspxl <rik.stevens@hotmail.com>
2019-12-09 09:18:08 +0100

    Update
    rechttrekking

--- subnet.tf
+++ subnet.tf
@@ -1,10 +1,31 @@
-resource "aws_subnet" "subnet-f391b794-subnet-f391b794" {
-    vpc_id                  = "${aws_vpc.default.id}"
-    cidr_block              = "172.31.1.0/16"
-    availability_zone       = "us-east-1d"
-    map_public_ip_on_launch = true
-
-    tags= {
+resource "aws_subnet" "Public_Subnet" {
+    vpc_id                  = "${aws_vpc.Fase3vpc.id}"
+    cidr_block              = "172.31.1.0/24"
+    tags = {
+        Name = "Public_Subnet"
+    }
+}
+resource "aws_subnet" "Private_Subnet" {
+    vpc_id                  = "${aws_vpc.Fase3vpc.id}"
+    cidr_block              = "172.31.2.0/24"
+    tags = {
+        Name = "Private_Subnet"
+    }
+}
+resource "aws_subnet" "RDSSubnet1" {
+    vpc_id                  = "${aws_vpc.Fase3vpc.id}"
+    cidr_block              = "172.31.3.0/24"
+    availability_zone       = "us-east-1a"
+    tags = {
+        Name = "RDSSubnet1"
+    }
+}
+resource "aws_subnet" "RDSSubnet2" {
+    vpc_id                  = "${aws_vpc.Fase3vpc.id}"
+    cidr_block              = "172.31.4.0/24"
+    availability_zone       = "us-east-1b"
+    tags = {
+        Name = "RDSSubnet2"
     }
 }
 
